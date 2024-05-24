e26e65185878f464923abe497536f3ddcb7500fa
Marat <zinatulinmarat@gmail.com>
2024-05-24 22:25:30 +0500

    next commit

--- src/features/navigation/ui/nav.tsx
+++ src/features/navigation/ui/nav.tsx
@@ -2,6 +2,7 @@ import { FC, useRef } from "react";
 import styles from './nav.module.scss';
 import clsx from "clsx";
 import { NavArrow } from "./nav-arrow/nav-arrow";
+import { Users } from "lucide-react";
 
 export const Nav: FC<{}> = () => {
 
@@ -30,6 +31,12 @@ export const Nav: FC<{}> = () => {
     <div className={clsx(styles['nav-content'])}>
       <div className={clsx(styles['nav-content-inner'])} ref={refInner}>
         <NavArrow toggleNav={toggleNav} refChildMethods={refChildMethods} />
+        <div className={clsx(styles['nav-content-inner__item'], 'flex', 'gap-1')}>
+          <div className="flex min-w-[30px] justify-center">
+            <Users color="var(--primary)" size={20} />
+          </div>
+          <p className="text-[16px] font-bold text-[var(--primary)]">Users</p>
+        </div>
       </div>
     </div>
   )
